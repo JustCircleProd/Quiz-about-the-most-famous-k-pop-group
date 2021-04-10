3eb6f99f9b4934d32861442c8e71b06bd1c020eb
VadimKarch <vadimkarworkmail@gmail.com>
2021-04-10 15:31:29 +0300

    created helpers folders, add directory with downloaded images to .gitignore

--- helpers/(helper)download_images.py
+++ helpers/(helper)download_images.py
@@ -4,9 +4,6 @@ import requests
 def download(query, limit):
     response = simp.simple_image_download()
     response.download(query, limit)
-    
-    return response.urls(query, limit)
-
 
 names = ('Kim Nam Joon', 'Kim Seok Jin', 'Min Yoon Gi', 'Jung Ho Seok', 'Park Ji Min', 'Kim Tae Hyung', 'Jeon Jung Kook')
 
