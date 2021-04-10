dbc1e76e591ecde86f26dcafb2181223a92f08f3
VadimKarch <vadimkarworkmail@gmail.com>
2021-04-10 19:03:18 +0300

    Fixed xtml file, added DataModel file

---
+++ DataModel.py
@@ -0,0 +1,8 @@
+import os
+
+class Db:
+    def __init__(self):
+        pass
+
+    def get_question(self):
+        pass
--- helpers/(helper)create_xml.py
+++ helpers/(helper)create_xml.py
@@ -6,17 +6,20 @@ try:
     root = ET.Element('data')
     id = 1
     for name in os.listdir(base_path):
-        answer = ET.Element('answer', name=name)
+        answer = ET.Element('answer')
+        answer.text = name
         images_path = os.path.join(base_path, name)
         for image_name in os.listdir(images_path):
+            question = ET.Element('question')
             id_json = ET.Element('id')
             id_json.text = str(id)
-            answer.append(id_json)
             image = ET.Element('image')
-            image.text = (open(os.path.join(images_path, image_name), 'rb').read())
-            answer.append(image)
+            image.text = str(open(os.path.join(images_path, image_name), 'rb').read())
+            question.append(id_json)
+            question.append(answer)
+            question.append(image)
             id += 1
-        root.append(answer)
+            root.append(question)
 
     open('resources\\data.xml', 'w').write(ET.tostring(root, encoding='unicode'))
 except Exception as e:
